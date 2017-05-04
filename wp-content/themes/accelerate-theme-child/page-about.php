212af248db250b37b7f9c6cc1a06a3059da4d64f
sarahskry <sarah.skrydstrup@gmail.com>
2017-05-04 17:06:24 -0400

    adding about page and styled the top main image and quote

---
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: About
+Template Post Type: page
+ */
+
+get_header(); ?>
+
+<section class="about-page">
+	<div class="site-content">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<div class='homepage-hero'>
+				<h3><span style="color:#45ac9d">Accelerate</span> is a strategy and marketing agency located in the
+				heart of NYC. Our goal is to build businesses by making our clients visible and
+				making their customers smile.</h3>
+			</div>
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .site-content -->
+</section><!-- .about-page -->
+
+
+<?php get_footer(); ?>
