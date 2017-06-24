e0381972e7cb6c97a54045ab0e71d9d91684e910
sarahskry <sarah.skrydstrup@gmail.com>
2017-06-24 14:26:02 -0400

    test change

--- wp-content/themes/accelerate-theme-child/page.php
+++ wp-content/themes/accelerate-theme-child/page.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The template for displaying all pages
- *
+ *THESE ARE MY WORDS!
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site will use a
